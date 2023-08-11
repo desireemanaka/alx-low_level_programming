5f8b5c342ff14aa82ce9735717dd2b2fa55c7659
desireemanaka <desireemanaka@gmail.com>
2023-08-11 11:43:22 -0700

    xoxo

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
 	char alp [52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		int i;
 	
-		for (i = 0; i > 52, i++)
+		for (i = 0; i < 52; i++)
 		{
 			putchar(alp[i]);
 		}
