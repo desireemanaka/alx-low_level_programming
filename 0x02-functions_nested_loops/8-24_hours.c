57ea5aa2d5c821788ae02f594b692b22ff7963b0
desireemanaka <desireemanaka@gmail.com>
2023-08-15 13:14:11 -0700

    xoxo

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,8 +18,10 @@ void jack_bauer(void)
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
-			_putchar('\');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 			j++;
 		}
+		i++;
 	}
 }
