be32cea891dca6cdc912a96d0f9fb1beda08cdd8
desireemanaka <desireemanaka@gmail.com>
2023-08-15 11:16:19 -0700

    xoxo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include <main.h>
- /**
-  * main - prints _putchar
-  *
-  * Return always 0 (Success)
-  */
+#include "main.h"
+/**
+ * main - print _putchar
+ * Return: 0
+ */
+
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	Return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
