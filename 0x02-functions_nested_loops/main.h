8af72edb1b02404ec4c569c6479928fc7b53d618
desireemanaka <desireemanaka@gmail.com>
2023-08-15 08:44:23 -0700

    xoxo

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#include main.h
+#ifndef main_h
+#define main_h
 
-int_putchar(chair);
+int_putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void print_alphabet_x10(void);
@@ -13,5 +14,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
