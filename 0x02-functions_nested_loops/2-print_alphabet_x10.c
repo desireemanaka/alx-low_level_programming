f2807461bd42f4b6c6ec2b414fd14aff5cfc8de5
desireemanaka <desireemanaka@gmail.com>
2023-08-16 10:48:32 -0700

    xoxo

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,12 @@
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  * followed by a new line
  */
-void print_alphabet_x10(void)
-{
+void print_alphabet_x10(void{
 	char ch;
 	int i;
+	
 	i = 0;
+	
 	while (i < 10)
 	{
 		ch = 'a';
@@ -19,5 +20,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 		i++;
 	}
-}
-	
+}	
