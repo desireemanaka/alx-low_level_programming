7d21ca15a9097c5bdf0254bd386bd982e7bd3518
desireemanaka <desireemanaka@gmail.com>
2023-08-17 05:52:46 -0700

    xoxo

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ * return: void
+ */
+void print_numbers(void)
+{
+	char c;
+	
+	fir (c = '0'; c <='9'; c++)
+	{
+		_putchar(c);
+	}
+}
