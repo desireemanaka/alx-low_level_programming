658ce8a920f3b8a076ecec29dc9e4b77800c3eed
desireemanaka <desireemanaka@gmail.com>
2023-08-17 07:56:07 -0700

    xoxo

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,27 +11,30 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; 1 <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz');
-		} else if (i == 1)
-		{
-			printf("%d", i);
+			printf("FizzBuzz");
 		}
 		else
 		{
 			printf("%d", i);
 		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
 	}
-	printf('\n');
+	printf("\n");
 
 	return (0);
 }
