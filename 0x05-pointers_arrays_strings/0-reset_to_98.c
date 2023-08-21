ac461ee4015556e8df15e11977e40a42d04f6035
desireemanaka <desireemanaka@gmail.com>
2023-08-21 06:38:05 -0700

    xoxo

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/* takes a pointer to an int as a parameter
+ * and updates the value it points to to 98
+ */
+{
+#include "main.c"
+
+void reset_to_98(int *n)
+	int n = 98;
+	int *p - &n;
+	printf(value to 98: %d\n", *p);
+
+	return 0;
+}
