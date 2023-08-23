31c35b17fc6e6329c97520dac0e34d4b3803a839
desireemanaka <desireemanaka@gmail.com>
2023-08-22 19:30:24 -0700

    xoxo

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,12 @@
-@include "main.h
+#include "main.h"
 
 /**
- *_atoi - converts a string to an integer
- *@s: string to be converted
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- *Return: the int converted from the string
+ * Return: the int converted from the string
  */
-int _antoi(char *s)
+int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
@@ -25,14 +25,14 @@ int _antoi(char *s)
 		if (s[i] == '-')
 			++d;
 
-		if (s[i] >= '0'&& s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if = (s[i +1] < '0'|| s[i + 1] > '9')
+			if = (s[i + 1] < '0' || s[i + 1] > '9');
 				break;
 			f = 0;
 		}
