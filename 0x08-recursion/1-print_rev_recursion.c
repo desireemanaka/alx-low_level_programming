b73fb25138f0374536c2f64e401c1dd72df2049f
desireemanaka <desireemanaka@gmail.com>
2023-08-30 10:40:46 -0700

    xoxo

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * 
+ *
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
@@ -8,7 +8,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursions(s +1);
+		_print_rev_recursions(s + 1);
 		_putchar(*s);
 	}
 }
