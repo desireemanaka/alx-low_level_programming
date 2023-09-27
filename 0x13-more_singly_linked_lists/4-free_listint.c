969b5636c13be7b960482f6240382d1e6bfb08a6
desireemanaka <desireemanaka@gmail.com>
2023-09-27 11:36:02 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,13 +5,16 @@
  * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
+
 {
 	listint_t *temp;
+
 	while (head)
 	{
 		temp = head->next;
 		free(head);
 		head = temp;
-		{
+	}
+
 }
 
